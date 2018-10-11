1ba818392c11c461cd07e6066801f9c3b3022e68
Simon Bungartz <simon.bungartz@zweitag.de>
2018-10-11 17:06:41 +0200

    JS in own file

---
+++ index.js
@@ -0,0 +1,8 @@
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
