c6cd5ce4509e5e9e4b69ca258d6507a6f728d66d
Simon Bungartz <simon.bungartz@zweitag.de>
2018-10-11 17:21:32 +0200

    Load data using hack data.js file magic

--- index.js
+++ index.js
@@ -5,4 +5,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+function loadData(data) {
+  const group = L.featureGroup();
+  group.addTo(map);
+
+  data.forEach(({lat, lon}) => {
+    L.marker([lat, lon]).addTo(group)
+  });
+
+  map.fitBounds(group.getBounds().pad(0.5));
+}
