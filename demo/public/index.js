dcd989b425efb94fdf0604d53ff9dbf2eed4b1ba
Simon Bungartz <simon.bungartz@zweitag.de>
2018-10-16 21:12:53 +0200

    Pass query parameter from URL bar to API

--- demo/public/index.js
+++ demo/public/index.js
@@ -23,5 +23,5 @@ function loadData(data) {
 }
 
 window.onload = () => {
-  fetch('/api/within?q=Münster').then(resp => resp.json()).then(loadData);
+  fetch(`/api/within${location.search}`).then(resp => resp.json()).then(loadData);
 };
