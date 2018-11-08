4299daa35a06ce61eafb8d46c36c632546a5356c
Tim Landwerth <tim.landwerth@posteo.de>
2018-11-08 18:56:08 +0100

    remove superfluous setting for vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
-  config.allow_http_connections_when_no_cassette = false
 end
 
 RSpec.configure do |config|
