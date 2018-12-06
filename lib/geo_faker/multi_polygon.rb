c9d21e5c3c5ca226d4d06739e21640b8d0fefbc2
Simon Bungartz <simon.bungartz@zweitag.de>
2018-12-06 17:29:46 +0100

    Add MultiPolygon#contains_point

--- lib/geo_faker/multi_polygon.rb
+++ lib/geo_faker/multi_polygon.rb
@@ -9,5 +9,32 @@ module GeoFaker
     def self.from_geojson(geojson)
       MultiPolygon.new([geojson['coordinates']])
     end
+
+    def contains_point(point)
+      point_in_poly(coordinates[0][0], point)
+    end
+
+    private
+
+    def point_in_poly(poly, point)
+      last_point = poly[-1]
+      oddNodes = false
+      y = point.lon
+      x = point.lat
+
+      poly.each do |p|
+        yi = p[0]
+        xi = p[1]
+        yj = last_point[0]
+        xj = last_point[1]
+        if yi < y && yj >= y ||
+            yj < y && yi >= y
+          oddNodes = !oddNodes if xi + (y - yi) / (yj - yi) * (xj - xi) < x
+        end
+        last_point = p
+      end
+
+      oddNodes
+    end
   end
 end
--- spec/geo_faker/multi_polygon_spec.rb
+++ spec/geo_faker/multi_polygon_spec.rb
@@ -1,66 +1,81 @@
 require 'geo_faker/multi_polygon'
+require 'geo_faker/point'
 
 # Test data taken from https://de.wikipedia.org/wiki/GeoJSON
 # on 6th of December 2018. The different polygons are drawn there.
-RSpec.describe GeoFaker::MultiPolygon do
-  shared_examples 'a factory method' do
-    it 'returns a MultiPolygon object' do
-      expect(subject).to be_a_kind_of(GeoFaker::MultiPolygon)
-    end
-  end
-
-  context 'with single Polygon without holes' do
-    let(:geojson) do
-      JSON.parse(<<~JSON)
-        {
-          "type": "Polygon",
-          "coordinates": [
-             [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
-          ]
-        }
-      JSON
-    end
-
-    describe '#from_geojson' do
-      subject { GeoFaker::MultiPolygon.from_geojson(geojson) }
-
-      it_behaves_like 'a factory method'
-
-      it 'converts to MultiPolygon by wrapping the coordinate list in another array' do
-        expect(subject).to have_attributes(coordinates: [
-          [
-            [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
-          ]
-        ])
+module GeoFaker
+  RSpec.describe MultiPolygon do
+    shared_examples 'a factory method' do
+      it 'returns a MultiPolygon object' do
+        expect(subject).to be_a_kind_of(MultiPolygon)
       end
     end
-  end
 
-  context 'with single Polygon without holes' do
-    let(:geojson) do
-      JSON.parse(<<~JSON)
-        {
-          "type": "Polygon",
-          "coordinates": [
-            [[35, 10], [45, 45], [15, 40], [10, 20], [35, 10]],
-            [[20, 30], [35, 35], [30, 20], [20, 30]]
-          ]
-        }
-      JSON
+    context 'with single Polygon without holes' do
+      let(:geojson) do
+        JSON.parse(<<~JSON)
+          {
+            "type": "Polygon",
+            "coordinates": [
+               [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
+            ]
+          }
+        JSON
+      end
+
+      describe '#from_geojson' do
+        subject { MultiPolygon.from_geojson(geojson) }
+
+        it_behaves_like 'a factory method'
+
+        it 'converts to MultiPolygon by wrapping the coordinate list in another array' do
+          expect(subject).to have_attributes(coordinates: [
+            [
+              [[30, 10], [40, 40], [20, 40], [10, 20], [30, 10]]
+            ]
+          ])
+        end
+      end
+
+      describe '#contains_point' do
+        let(:multi_polygon) { MultiPolygon.from_geojson(geojson) }
+
+        it 'returns true for contained point' do
+          expect(multi_polygon.contains_point(Point.new(lat: 35, lon: 20))).to be(true)
+        end
+
+        it 'returns false for outer point' do
+          expect(multi_polygon.contains_point(Point.new(lat: 20, lon: 35))).to be(false)
+        end
+      end
     end
 
-    describe '#from_geojson' do
-      subject { GeoFaker::MultiPolygon.from_geojson(geojson) }
+    context 'with single Polygon without holes' do
+      let(:geojson) do
+        JSON.parse(<<~JSON)
+          {
+            "type": "Polygon",
+            "coordinates": [
+              [[35, 10], [45, 45], [15, 40], [10, 20], [35, 10]],
+              [[20, 30], [35, 35], [30, 20], [20, 30]]
+            ]
+          }
+        JSON
+      end
 
-      it_behaves_like 'a factory method'
+      describe '#from_geojson' do
+        subject { MultiPolygon.from_geojson(geojson) }
 
-      it 'converts to MultiPolygon by wrapping the coordinate list in another array' do
-        expect(subject).to have_attributes(coordinates: [
-          [
-            [[35, 10], [45, 45], [15, 40], [10, 20], [35, 10]],
-            [[20, 30], [35, 35], [30, 20], [20, 30]]
-          ]
-        ])
+        it_behaves_like 'a factory method'
+
+        it 'converts to MultiPolygon by wrapping the coordinate list in another array' do
+          expect(subject).to have_attributes(coordinates: [
+            [
+              [[35, 10], [45, 45], [15, 40], [10, 20], [35, 10]],
+              [[20, 30], [35, 35], [30, 20], [20, 30]]
+            ]
+          ])
+        end
       end
     end
   end
