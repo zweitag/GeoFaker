5c97ed545f4b9887db9c12a66cb19a8c3525bcc4
Simon Bungartz <simon.bungartz@zweitag.de>
2018-12-26 22:53:23 +0100

    Add module Geometry according to directory structure

--- lib/geo_faker/geometry/point.rb
+++ lib/geo_faker/geometry/point.rb
@@ -1,17 +1,19 @@
 module GeoFaker
-  class Point
-    attr_reader :lat, :lon
+  module Geometry
+    class Point
+      attr_reader :lat, :lon
 
-    def initialize(lat:, lon:)
-      @lat = lat
-      @lon = lon
-    end
+      def initialize(lat:, lon:)
+        @lat = lat
+        @lon = lon
+      end
 
-    def to_json(*opts)
-      {
-        lat: lat,
-        lon: lon,
-      }.to_json(opts)
+      def to_json(*opts)
+        {
+          lat: lat,
+          lon: lon,
+        }.to_json(opts)
+      end
     end
   end
 end
